554661eac904e783354c069ae7a778474c6feeea
shankarb18 <shankarbhore7993@gmail.com>
2022-11-02 12:44:42 +0530

    updated file

--- prime.js
+++ prime.js
@@ -1,8 +1,20 @@
 function checkPrime(num){
 let counter=0;
-for(let i=0;i<=num;i++){
+for(let i=1;i<=num;i++){
 if(num%i==0){
 counter++;
+}}
+if(counter==2){
+return true;
 }
+ return false;
 }
-console.log(num);
+
+
+
+let ans=checkPrime(13);
+if(ans==true){
+console.log('Prime Number');
+}else{
+console.log('Not Prime Number')
+}
